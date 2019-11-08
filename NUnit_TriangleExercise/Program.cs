e35d6a10d1d3450c97231f39e1eb6a009ecd7f2c
Heikki Luukkonen <heikki1808@student.hamk.fi>
2019-11-08 19:54:40 +0200

    Main program ready.

--- NUnit_TriangleExercise/Program.cs
+++ NUnit_TriangleExercise/Program.cs
@@ -4,11 +4,41 @@
 
 namespace NUnit_TriangleExercise
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            int side1, side2, side3;
+
+
+            Console.WriteLine("Insert side 1 of triangle: ");
+            side1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert side 2 of triangle: ");
+            side2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insert side 3 of triangle: ");
+            side3 = int.Parse(Console.ReadLine());
+
+            Checker Checker = new Checker();
+            Checker.CheckSides(side1, side2, side3);
+
+        }
+    }
+    class Checker
+    {
+        public void CheckSides(int x, int y, int z)
+        {
+            if(x == y && y == z)
+            {
+                Console.WriteLine("All triangle sides are equal.");
+            }
+            else if(x == y || x == z || y == z)
+            {
+                Console.WriteLine("Two of the triangle sides are equal.");
+            }
+            else
+            {
+                Console.WriteLine("All of the triangle sides are unequal.");
+            }   
         }
     }
 }
